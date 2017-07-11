dc5bfc1df1e041b3ba3e7da2726e86f18b0663a6
jpatricia <janice_patricia@live.com>
2017-07-11 18:23:51 -0400

    added menu list items, no image yet

--- app/src/main/java/chemotracker/fydp/chemotrackerandroid/MenuFragment.java
+++ app/src/main/java/chemotracker/fydp/chemotrackerandroid/MenuFragment.java
@@ -5,15 +5,27 @@ package chemotracker.fydp.chemotrackerandroid;
  */
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-    public class MenuFragment extends Fragment {
-        public MenuFragment() {
-        }
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.menu_fragment, container, false);
-        }
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MenuFragment extends ListFragment {
+    public MenuFragment() {
     }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.menu_fragment, container, false);
+        String[] items = new String[] {"Profile" , "Journal", "Forums" , "Settings", "Log Out"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+        setListAdapter(adapter);
+        return rootView;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO implement some logic
+    }
+}
