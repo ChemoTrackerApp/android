5077dc6316624dd50a9030aef8361178601065c5
Janice Patricia <janice_patricia@live.com>
2017-09-18 22:24:16 -0400

    changed layout of symptom tracking fragment

--- app/src/main/java/com/innovare/chemotracker/TrackAdapter.java
+++ app/src/main/java/com/innovare/chemotracker/TrackAdapter.java
@@ -1,6 +1,8 @@
 package com.innovare.chemotracker;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,72 +13,123 @@ import android.widget.TextView;
 /**
  * Created by Janice on 7/17/2017.
  */
+//
+//public class TrackAdapter extends BaseAdapter {
+//	private Context context;
+//	private final String[] symptomValues;
+//
+//	public TrackAdapter(Context context, String[] symptomValues) {
+//		this.context = context;
+//		this.symptomValues = symptomValues;
+//	}
+//
+//	public View getView(int position, View convertView, ViewGroup parent) {
+//
+//		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//		View grid;
+//
+//		if (convertView == null) {
+//
+//			grid = new View(context);
+//
+//			// get layout from mobile.xml
+//			grid = inflater.inflate(R.layout.track_symptoms, null);
+//
+//			// set value into textview
+//			TextView textView = (TextView) grid
+//					.findViewById(R.id.symptom_label);
+//			textView.setText(symptomValues[position]);
+//
+//			// set image based on selected text
+//			ImageView imageView = (ImageView) grid
+//					.findViewById(R.id.symptom_image);
+//
+//			String symptom = symptomValues[position];
+//
+//			if (symptom.equals("Nausea")) {
+//				imageView.setImageResource(R.drawable.nausea_icon);
+//			} else if (symptom.equals("Fatigue")) {
+//				imageView.setImageResource(R.drawable.fatigue_icon);
+//			} else if (symptom.equals("Constipation")) {
+//				imageView.setImageResource(R.drawable.constipation_icon);
+//			} else if(symptom.equals("Vomit")){
+//				imageView.setImageResource(R.drawable.vomit_icon);
+//			} else if(symptom.equals("Diarrhea")){
+//				imageView.setImageResource(R.drawable.diarrhea_icon);
+//			}
+//
+//		} else {
+//			grid = (View) convertView;
+//		}
+//
+//		return grid;
+//	}
 
-public class TrackAdapter extends BaseAdapter {
-	private Context context;
-	private final String[] symptomValues;
 
-	public TrackAdapter(Context context, String[] symptomValues) {
-		this.context = context;
-		this.symptomValues = symptomValues;
-	}
+	/**
+	 * Provide views to RecyclerView with data from mDataSet.
+	 */
+	public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.ViewHolder> {
+		private static final String TAG = "CustomAdapter";
 
-	public View getView(int position, View convertView, ViewGroup parent) {
+		private String[] mDataSet;
 
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		/**
+		 * Provide a reference to the type of views that you are using (custom ViewHolder)
+		 */
+		public static class ViewHolder extends RecyclerView.ViewHolder {
+			private final TextView textView;
 
-		View grid;
-
-		if (convertView == null) {
-
-			grid = new View(context);
-
-			// get layout from mobile.xml
-			grid = inflater.inflate(R.layout.track_symptoms, null);
-
-			// set value into textview
-			TextView textView = (TextView) grid
-					.findViewById(R.id.symptom_label);
-			textView.setText(symptomValues[position]);
-
-			// set image based on selected text
-			ImageView imageView = (ImageView) grid
-					.findViewById(R.id.symptom_image);
-
-			String symptom = symptomValues[position];
-
-			if (symptom.equals("Nausea")) {
-				imageView.setImageResource(R.drawable.nausea_icon);
-			} else if (symptom.equals("Fatigue")) {
-				imageView.setImageResource(R.drawable.fatigue_icon);
-			} else if (symptom.equals("Constipation")) {
-				imageView.setImageResource(R.drawable.constipation_icon);
-			} else if(symptom.equals("Vomit")){
-				imageView.setImageResource(R.drawable.vomit_icon);
-			} else if(symptom.equals("Diarrhea")){
-				imageView.setImageResource(R.drawable.diarrhea_icon);
+			public ViewHolder(View v) {
+				super(v);
+				// Define click listener for the ViewHolder's View.
+				v.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
+					}
+				});
+				textView = (TextView) v.findViewById(R.id.textView);
 			}
 
-		} else {
-			grid = (View) convertView;
+			public TextView getTextView() {
+				return textView;
+			}
 		}
 
-		return grid;
-	}
+		/**
+		 * Initialize the dataset of the Adapter.
+		 *
+		 * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+		 */
+		public TrackAdapter(String[] dataSet) {
+			mDataSet = dataSet;
+		}
 
-	@Override
-	public int getCount() {
-		return symptomValues.length;
-	}
+		// Create new views (invoked by the layout manager)
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+			// Create a new view.
+			View v = LayoutInflater.from(viewGroup.getContext())
+					.inflate(R.layout.track_recycler, viewGroup, false);
 
-	@Override
-	public Object getItem(int position) {
-		return null;
-	}
+			return new ViewHolder(v);
+		}
 
-	@Override
-	public long getItemId(int position) {
-		return 0;
-	}
+		// Replace the contents of a view (invoked by the layout manager)
+		@Override
+		public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+			Log.d(TAG, "Element " + position + " set.");
 
-}
+			// Get element from your dataset at this position and replace the contents of the view
+			// with that element
+			viewHolder.getTextView().setText(mDataSet[position]);
+		}
+
+		// Return the size of your dataset (invoked by the layout manager)
+		@Override
+		public int getItemCount() {
+			return mDataSet.length;
+		}
+	}
--- app/src/main/java/com/innovare/chemotracker/TrackFragment.java
+++ app/src/main/java/com/innovare/chemotracker/TrackFragment.java
@@ -6,6 +6,8 @@ package com.innovare.chemotracker;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,25 +18,82 @@ import android.widget.Toast;
 
 public class TrackFragment extends Fragment {
 
-    static final String[] SYMPTOMS = new String[] {
-            "Nausea", "Fatigue", "Constipation", "Vomit", "Diarrhea"
-    };
+//    static final String[] SYMPTOMS = new String[] {
+//            "Nausea", "Fatigue", "Constipation", "Vomit", "Diarrhea"
+//    };
+//
+//    public TrackFragment() {
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.track_fragment, container, false);
+//        GridView grid = (GridView) view.findViewById(R.id.track_gridView);
+//
+//        grid.setAdapter(new TrackAdapter(this.getContext(), SYMPTOMS));
+//        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+//
+//            }
+//        });
+//        return view;
+//    }
 
-    public TrackFragment() {
+    private static final String TAG = "TrackFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+    private static final int DATASET_COUNT = 5;
+
+
+    protected RecyclerView mRecyclerView;
+    protected TrackAdapter trackAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected String[] mDataset;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize dataset, this data would usually come from a local content provider or
+        // remote server.
+        initDataset();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.track_fragment, container, false);
-        GridView grid = (GridView) view.findViewById(R.id.track_gridView);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.track_fragment, container, false);
+        rootView.setTag(TAG);
 
-        grid.setAdapter(new TrackAdapter(this.getContext(), SYMPTOMS));
-        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
 
-            }
-        });
-        return view;
+        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
+        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
+        // elements are laid out.
+        mLayoutManager = new LinearLayoutManager(getActivity());
+
+        trackAdapter = new TrackAdapter(mDataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(trackAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+    }
+
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
     }
 }
 
